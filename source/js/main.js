e7540bfa91731a624ab5d80dae9ae145afc5d39b
Zijun Yang <zijunhz@126.com>
2025-10-26 10:59:11 -0400

    enable darkreader

--- source/js/main.js
+++ source/js/main.js
@@ -3,12 +3,12 @@
 var hasCurrent = false;
 var isindexpage = true;
 $('#main-nav > li').each(function () {
-	if(isindexpage){
+	if (isindexpage) {
 		isindexpage = false;
 		return true;
-	  }
+	}
 	var url = window.location.href;
-	if(url.toUpperCase().indexOf($(this).attr("linktext").trim().toUpperCase()) != -1){
+	if (url.toUpperCase().indexOf($(this).attr("linktext").trim().toUpperCase()) != -1) {
 		$(this).addClass('current-menu-item current_page_item');
 		hasCurrent = true;
 	} else {
@@ -20,6 +20,14 @@ if (!hasCurrent) {
 	$('#main-nav > li:first').addClass('current-menu-item current_page_item');
 }
 
+// enable dark reader
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/darkreader/darkreader.min.js';
+script.onload = function () {
+	DarkReader.auto();
+};
+document.head.appendChild(script);
+
 
 
 // article toc
@@ -27,31 +35,31 @@ var toc = document.getElementById('toc')
 
 if (toc != null) {
 	window.addEventListener("scroll", scrollcatelogHandler);
-	var tocPosition = 194+25;
+	var tocPosition = 194 + 25;
 
-	function scrollcatelogHandler(e) {
-		 var event = e || window.event,
-		     target = event.target || event.srcElement;
-		 var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		 if (scrollTop > tocPosition) {
-		     toc.classList.add("toc-fixed");
-		 } else {
-		     toc.classList.remove("toc-fixed");
-		 }
+	function scrollcatelogHandler (e) {
+		var event = e || window.event,
+			target = event.target || event.srcElement;
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		if (scrollTop > tocPosition) {
+			toc.classList.add("toc-fixed");
+		} else {
+			toc.classList.remove("toc-fixed");
+		}
 	}
 }
 
 
-$('#main-navigation').on('click', function(){
-    if ($('#main-navigation').hasClass('main-navigation-open')){
-      $('#main-navigation').removeClass('main-navigation-open');
-    } else {
-      $('#main-navigation').addClass('main-navigation-open');
-    }
-  });
+$('#main-navigation').on('click', function () {
+	if ($('#main-navigation').hasClass('main-navigation-open')) {
+		$('#main-navigation').removeClass('main-navigation-open');
+	} else {
+		$('#main-navigation').addClass('main-navigation-open');
+	}
+});
 
-$('#content').on('click', function(){
-    if ($('#main-navigation').hasClass('main-navigation-open')){
-      $('#main-navigation').removeClass('main-navigation-open');
-    }
-  });
+$('#content').on('click', function () {
+	if ($('#main-navigation').hasClass('main-navigation-open')) {
+		$('#main-navigation').removeClass('main-navigation-open');
+	}
+});
